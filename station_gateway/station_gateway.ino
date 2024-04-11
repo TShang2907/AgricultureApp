f04d22be18736db2d93c5d0571eeb036e17d220f
NguyenSangtngh <79956221+NguyenSangtngh@users.noreply.github.com>
2024-04-11 22:37:04 +0700

    Update mqqt python & station_gateway

--- station_gateway/station_gateway.ino
+++ station_gateway/station_gateway.ino
@@ -4,7 +4,7 @@
 #include "Unit_4RELAY.h"
 #include <ArduinoJson.h>
 
-#define SSID_WIFI "HUYNH NGOC"  
+#define SSID_WIFI "HUYNH NGOC"
 #define PASS_WIFI "HN200912@"
 #define MQTT_SERVER "mqttserver.tk"
 #define USERNAME "innovation"
@@ -18,27 +18,22 @@
 
 UNIT_4RELAY relay;
 // Class MyMQTT
-MyMQTT mqtt(MQTT_SERVER,USERNAME,PASSWORD,BUFFER_SIZE); //Initialize MQTT Server
+MyMQTT mqtt(MQTT_SERVER, USERNAME, PASSWORD, BUFFER_SIZE); // Initialize MQTT Server
 
 StaticJsonDocument<500> doc;
 StaticJsonDocument<300> sensors_json;
 StaticJsonDocument<100> relay_json;
 uint8_t relayArray[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-
-
-
-int value=0;
-int relay_id=0;
-
-
-// Function Setup WIFI
-void setupWifi(){
+// Setup wifi
+void setupWifi()
+{
   M5.Lcd.printf("Connecting to %s", SSID_WIFI);
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(SSID_WIFI, PASS_WIFI);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     M5.Lcd.print(".");
   }
@@ -46,168 +41,147 @@ void setupWifi(){
 }
 
 // Function Receive Data From MQTT Broker
-void myReceiveFunction (char* topic, byte* payload, unsigned int length){
+void myReceiveFunction(char *topic, byte *payload, unsigned int length)
+{
   M5.Lcd.println("Message arrived from :");
   M5.Lcd.println(topic);
   // Convert data to string
-  char* data_sensor=reinterpret_cast<char*>(payload);
-  //M5.Lcd.println(data_sensor);
-  
-  
-  //Serial1.write(data_sensor);
-  // Deserialize the JSON document
-  DeserializationError error = deserializeJson(doc,data_sensor);
-
-  //serializeJson(doc, Serial1);
-
+  char *data_sensor = reinterpret_cast<char *>(payload);
+  //  Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, data_sensor);
   // Test if parsing succeeds.
-  if (error) {
+  if (error)
+  {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.f_str());
     return;
   }
 
-
   JsonArray sensors = doc["sensors"].as<JsonArray>();
 
-  
+  if (doc["station_id"] == "VALVE_0001" || doc["station_id"] == "PUMP_0001")
+  {
 
-  if(doc["station_id"]=="VALVE_0001" || doc["station_id"]=="PUMP_0001" ){
-
-    int index=0;
+    int index = 0;
     relay_json["station_id"] = doc["station_id"];
-    
-    if(doc["station_id"]=="PUMP_0001"){
-      index=index+3;
+
+    if (doc["station_id"] == "PUMP_0001")
+    {
+      index = index + 3;
     }
 
-    for (JsonVariant sensor : sensors) {
-        uint8_t value=(uint8_t)sensor["sensor_value"];
-        M5.Lcd.print(value);
-        if(relayArray[index]!=value){
-          relayArray[index]=value;
-          relay_json["index"]=index;
-          relay_json["value"]=relayArray[index];
-          serializeJson(relay_json, Serial1);
-          delay(1000);
+    for (JsonVariant sensor : sensors)
+    {
+      uint8_t value = (uint8_t)sensor["sensor_value"];
+      M5.Lcd.print(value);
+      if (relayArray[index] != value)
+      {
+        relayArray[index] = value; // Save to relay array
+
+        relay_json["index"] = index; // Add to json
+        relay_json["value"] = relayArray[index];
+
+        // Send data to 4 relay
+        if (index > 1)
+        {
+          relay.relayWrite(index - 4, value);
         }
-        index++;   
+        else
+        {
+          relay.relayWrite(index, value);
+        }
+
+        M5.Lcd.printf("Realay%d: ", index);
+        M5.Lcd.printf("%d\n", value);
+
+        // Send data to display
+        serializeJson(relay_json, Serial1);
+        delay(1000);
+      }
+      index++;
     }
   }
 
   // Monitor
-  
-  if(doc["station_id"]=="air_0002"){
+  if (doc["station_id"] == "air_0002")
+  {
+    int index_sensor = 0;
+    sensors_json["station_id"] = doc["station_id"];
 
-    int index_sensor=0;     
-    sensors_json["station_id"]=doc["station_id"];
+    for (JsonVariant sensor : sensors)
+    {
+      sensors_json["index"] = index_sensor;
+      sensors_json["value"] = sensor["value"];
 
-    for (JsonVariant sensor : sensors) {
-      
-        sensors_json["index"]=index_sensor;
-        sensors_json["value"]=sensor["value"];
-
-        serializeJson(sensors_json, Serial1);
-        delay(1000);
-        index_sensor++;   
+      serializeJson(sensors_json, Serial1);
+      delay(1000);
+      index_sensor++;
     }
   }
-
 }
 
- 
+/////////////////////////////////////////////
 
+void setup()
+{
+  // Initialize M5Station.
+  M5.begin();
 
-void setup() {
-  // Initialize M5Station. 
-  M5.begin();            
+  // Setup UART 1
   Serial.begin(115200);
-  //Đặt dung lượng bộ đệm cho RX và TX
-  Serial1.setTxBufferSize(SERIAL_BUFFER_SIZE);
-  Serial1.setRxBufferSize(SERIAL_BUFFER_SIZE);
-  Serial1.begin(115200, SERIAL_8N1, 13,14);
+  Serial1.begin(115200, SERIAL_8N1, 13, 14);
 
   // Initialize Wifi
-  setupWifi();            
+  setupWifi();
 
   // Connect to MQTT Broker
-  if(mqtt.connectToMQTT()){         
+  if (mqtt.connectToMQTT())
+  {
     M5.Lcd.print("Successful MQTT connection\n");
-  }else{
+  }
+  else
+  {
     M5.Lcd.print("Failed MQTT connection\n");
   }
 
   // Subscribe to VALVE
-  if(mqtt.subscribe(VALVE_TOPIC)){
-    M5.Lcd.print("Successful MQTT subscribing\n");
-  }else{
-    M5.Lcd.print("Failed MQTT subscribing\n");
-  }
+  mqtt.subscribe(VALVE_TOPIC);
   // Subscribe to PUMP
-  if(mqtt.subscribe(PUMP_TOPIC)){
-    M5.Lcd.print("Successful MQTT subscribing\n");
-  }else{
-    M5.Lcd.print("Failed MQTT subscribing\n");
-  }
-  //Subscribe to MONITOR
-  if(mqtt.subscribe(SENSOR_DATA_TOPIC)){
-    M5.Lcd.print("Successful MQTT subscribing\n");
-  }else{
-    M5.Lcd.print("Failed MQTT subscribing\n");
-  }
+  mqtt.subscribe(PUMP_TOPIC);
+  // Subscribe to MONITOR
+  mqtt.subscribe(SENSOR_DATA_TOPIC);
 
-  // Set Receive Function 
+  // Set Receive Function Mqtt
   mqtt.setReceiveFunction(myReceiveFunction);
+
   // Initialize Unit_4Relay.
-  if(relay.begin(&Wire, 32, 33)) {   
+  if (relay.begin(&Wire, 32, 33))
+  {
     M5.Lcd.printf("Successful Relay connection\n");
-    relay.Init(1);  // Async = 0, Sync = 1  
-  }else
+    relay.Init(1); // Async = 0, Sync = 1
+  }
+  else
   {
     M5.Lcd.printf("Failed Relay connection\n");
   }
-  
-  
 }
 
-void loop() {
-  
-  if (Serial1.available()>0){
-     String receivedData = "";
-    while (Serial1.available() > 0) {
+////////////////////////////////////////////
+
+void loop()
+{
+  M5.update();         // Update state button
+  mqtt.checkConnect(); // Check connected mqtt
+
+  // Check returned message uart
+  if (Serial1.available() > 0)
+  {
+    String receivedData = "";
+    while (Serial1.available() > 0)
+    {
       char receivedChar = (char)Serial1.read();
       receivedData += receivedChar;
     }
-    // Xử lý dữ liệu nhận được ở đây
-    M5.Lcd.println("Received data from Serial1: " + receivedData);
-    }
-    
-  if (M5.BtnC.wasPressed()) {
-    //Serial1.write("hello");
-    
-    if(serializeJson(relay_json, Serial1)) {M5.Lcd.printf("Sucessful Sending\n");}
-
+    M5.Lcd.println("Returned message: " + receivedData);
   }
-
-  mqtt.checkConnect();
- 
- 
-  if (M5.BtnA.wasPressed()) {
-    if(relay_id<3){ relay_id++;}
-    else{relay_id=0;}
-    
-  }
-
-  if (M5.BtnB.wasPressed()) {
-    if(value==0){
-      value=1;
-    }else{
-      value=0;
-    }
-    relay.relayWrite(relay_id,value);
-    M5.Lcd.printf("Realay%d: ",relay_id);
-    M5.Lcd.printf("%d\n",value);
-  }
-  M5.update();
-
 }
